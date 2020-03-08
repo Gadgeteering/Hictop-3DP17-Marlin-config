70d0251dd35bf39688aabf5f5af6dca959c7c16c
Gadgeteer <pmhunt1972@gmail.com>
2020-03-08 10:04:34 +0000

    3DP-12   Z-Axis needs inverting
    3DP-12   Z-Axis needs inverting

--- Configuration.h
+++ Configuration.h
@@ -1055,7 +1055,7 @@
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR false
 #define INVERT_Y_DIR false //Hictop
-#define INVERT_Z_DIR false
+#define INVERT_Z_DIR true // 3DP-12 needs invering
 
 // @section extruder
 
